e6e007bd928001fb8b6c1079b5ec4d4253da9eb7
cpele <geekarist@users.noreply.github.com>
2018-07-04 06:52:59 +0200

    Fix build, add EditText

--- app/src/main/java/me/cpele/myapplication/ui/main/MainViewModel.kt
+++ app/src/main/java/me/cpele/myapplication/ui/main/MainViewModel.kt
@@ -1,7 +1,12 @@
 package me.cpele.myapplication.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    var name = "Friend"
+
+    var _name = MutableLiveData<String>()
+    val name: LiveData<String>
+        get() = _name
 }
